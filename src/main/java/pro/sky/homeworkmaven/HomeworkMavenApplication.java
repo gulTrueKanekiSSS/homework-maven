37a6c770262e5cabaa5e74b9e51e17e709c3dbec
Дмитрий Захаров <zds.vip1221@gmail.com>
2024-12-09 13:24:57 +0300

    Initial commit

---
+++ src/main/java/pro/sky/homeworkmaven/HomeworkMavenApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkMavenApplication.class, args);
+	}
+
+}
